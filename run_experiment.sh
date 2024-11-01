53d66dea4fc5378bf907a55130dabf208f5a70d0
linlany <465057951@qq.com>
2024-11-01 19:39:53 +0800

    update script

--- run_experiment.sh
+++ run_experiment.sh
@@ -3,17 +3,17 @@
 python main.py \
     --config-path configs/class \
     --config-name imagenet100_10-10.yaml \
-    dataset_root="/defaultShare/pubdata/imagenet/" \
+    dataset_root="[imagenet_1k_path]" \
     class_order="class_orders/imagenet100.yaml"
 
 python main.py \
     --config-path configs/class \
     --config-name imagenet_r_20-20.yaml \
-    dataset_root="/huanglinlan/back_code/imagenet-r-split/" \
+    dataset_root="[imagenet_r_path]" \
     class_order="class_orders/imagenet_R_order.yaml"
 
 python main.py \
     --config-path configs/class \
     --config-name cifar100_10-10.yaml \
-    dataset_root="/defaultShare/archive/huanglinlan/" \
+    dataset_root="[cifar100_path]" \
     class_order="class_orders/cifar100_order.yaml"
